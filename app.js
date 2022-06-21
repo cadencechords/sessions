049d804e1e981778a52b4ca8e14205a1cb7b5e8b
Andrew Sidorchuk <andrew.sidorchuk@lifeway.com>
2022-06-21 15:11:53 -0400

    Updated cors

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ require('dotenv').config();
 const app = require('express')();
 const cors = require('cors');
 app.use(cors());
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', '*');
+  res.setHeader('Access-Control-Allow-Headers', '*');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  next();
+});
 const http = require('http').Server(app);
 const io = require('socket.io')(http, {
   cors: {
