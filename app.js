31cd4437d7748c2223d1ad566f4489bb16e55907
Andrew Sidorchuk <andrew.sidorchuk@lifeway.com>
2022-06-22 19:51:36 -0400

    Added socketio admin

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http, {
   cors: {
-    origin: 'https://app.cadencechords.com',
+    origin: ['https://app.cadencechords.com', 'https://admin.socket.io'],
     methods: ['GET', 'POST'],
+    credentials: true,
   },
 });
+const { instrument } = require('@socket.io/admin-ui');
 const port = process.env.PORT || 3002;
 let activeSessions = {};
 const { disconnect } = require('./socket/connectionHandler')(
@@ -26,6 +28,17 @@ io.on('connection', socket => {
   socket.on('perform change song', performChangeSong);
 
   socket.on('end session', endSession);
+});
+
+instrument(io, {
+  auth:
+    process.env.ENV !== 'dev'
+      ? false
+      : {
+          type: 'basic',
+          username: process.env.SOCKETIO_ADMIN_USER,
+          password: process.env.SOCKETIO_ADMIN_PASSWORD,
+        },
 });
 
 http.listen(port, async () => {
